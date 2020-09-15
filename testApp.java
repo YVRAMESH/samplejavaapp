78071956afd8e65ca8ce94c879351f3ad3bd1ab8
YVRAMESH <67633617+YVRAMESH@users.noreply.github.com>
2020-09-15 22:39:52 +0530

    testApp.java
    test

---
+++ testApp.java
@@ -0,0 +1,15 @@
+class A 
+{
+	static int j=100;
+ int i=50;
+	public static void main(String[] args) 
+	{
+A b=new A();
+System.out.println(b.j);//100
+System.out.println(b.i);//50
+A d=null;
+System.out.println(d.j);//100
+System.out.println(d.i);//null pointer Exception
+
+	}
+}
